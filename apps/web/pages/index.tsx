09596070fe98267df27ca4fdc08fd53686ea8d9e
Carlos Cabral <ccabral81@hotmail.com>
2026-01-29 21:44:30 -0500

    UI changes around language enhancements adn improvements

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,15 +3,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div style={{ padding: 20 }}>
-      <h1>DOCULEA</h1>
+      <h1>DOCU-LEA</h1>
       <p>Local dev home page</p>
 
       <Link href="/doculea-test">
         <button style={{ padding: 10, border: "1px solid black", borderRadius: 8 }}>
-          Open DOCULEA Test
+          Open DOCU-LEA
         </button>
       </Link>
     </div>
   );
 }
-
